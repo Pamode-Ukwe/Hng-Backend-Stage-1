316a0c74a8636043a155ad0ccdce53f96f2a1694
Pamode-Ukwe <ebenezerukwe@gmail.com>
2023-09-11 08:33:46 +0100

    New Files
    Contains source code and requirements

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from datetime import datetime
+import pytz
+
+app = FastAPI()
+
+@app.get("/")
+def home(slack_name:str = "Pamode", track:str = "backend"):
+    now = datetime.now(pytz.UTC)
+    utc_time = now.strftime("%Y-%m-%dT%H:%M:%SZ")
+    current_day = now.strftime("%A")
+    return {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": "https://github.com/Pamode-Ukwe/Hng-Backend-Stage-1/blob/main/stage1/main.py",
+        "github_repo_url": "https://github.com/Pamode-Ukwe/Hng-Backend-Stage-1",
+        "status_code": 200
+    }
